46d287cc3ee4af84c6781204422f136d0ae484f1
nileshgnagepatil <nileshgnagepatil@outlook.com>
2024-06-19 13:46:53 +0100

    Error Controller

---
+++ src/SFA.DAS.TeachInFurtherEducation.UnitTests/Web/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.TeachInFurtherEducation.Web.Controllers;
+using FakeItEasy;
+using Xunit;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+using SFA.DAS.TeachInFurtherEducation.Contentful.Services.Interfaces;
+
+namespace SFA.DAS.FindEmploymentSchemes.UnitTests.Web.Controllers
+{
+    public class ErrorControllerTests
+    {
+        public ILogger<ErrorController> Logger { get; set; }
+        private ErrorController ErrorController { get; set; }
+
+        private IContentService IContentService { get; set; }
+
+        public ErrorControllerTests()
+        {
+            Logger = A.Fake<ILogger<ErrorController>>();
+
+            IContentService = A.Fake<IContentService>();
+
+            ErrorController = new ErrorController(Logger, IContentService)
+            {
+                ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext() }
+            };
+        }
+
+        [Fact]
+        public void PageNotFound_UsesPageNotFoundViewTest()
+        {
+            var result = ErrorController.PageNotFound();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            // ViewName == null means use Error/PageNotFound.cshtml, which is what we want
+            Assert.Null(viewResult.ViewName);
+        }
+
+        [Fact]
+        public void ApplicationError_UsesApplicationErrorViewTest()
+        {
+            var result = ErrorController.ApplicationError();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            // ViewName == null means use Error/ApplicationError.cshtml, which is what we want
+            Assert.Null(viewResult.ViewName);
+        }
+        [Fact]
+        public void ApplicationError_LogsErrorTest()
+        {
+            ErrorController.ApplicationError();
+
+            A.CallTo(Logger)
+                .Where(call => call.Method.Name == "Log" && call.GetArgument<LogLevel>(0) == LogLevel.Error)
+                .MustHaveHappened();
+        }
+    }
+}
---
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Controllers/ErrorController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.TeachInFurtherEducation.Contentful.Services.Interfaces;
+using SFA.DAS.TeachInFurtherEducation.Web.Infrastructure;
+using SFA.DAS.TeachInFurtherEducation.Web.Models;
+using SFA.DAS.TeachInFurtherEducation.Web.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.TeachInFurtherEducation.Web.Controllers
+{
+    [Route("error")]
+    public class ErrorController : Controller
+    {
+        private readonly ILogger<ErrorController> _log;
+
+        private static readonly LayoutModel LayoutModel = new LayoutModel();
+
+        private readonly IContentService _contentService;
+
+        public ErrorController(ILogger<ErrorController> logger, IContentService contentService)
+        {
+
+            _log = logger;
+
+            _contentService = contentService;
+
+        }
+
+        [Route("404", Name = RouteNames.Error404)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult PageNotFound()
+        {
+
+            try
+            {
+
+                LayoutModel.footerLinks = _contentService.Content.FooterLinks;
+                LayoutModel.MenuItems = _contentService.Content.MenuItems;
+
+                return View(LayoutModel);
+
+            }
+            catch(Exception _exception)
+            {
+
+                _log.LogError(_exception, "Unable to get model with populated footer");
+
+                return View(LayoutModel);
+
+            }
+
+        }
+
+        [Route("500", Name = RouteNames.Error500)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ApplicationError()
+        {
+            IExceptionHandlerPathFeature? feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            _log.LogError($"500 result at {feature?.Path ?? "{unknown}"}", feature?.Error);
+            return View(LayoutModel);
+        }
+    }
+}
