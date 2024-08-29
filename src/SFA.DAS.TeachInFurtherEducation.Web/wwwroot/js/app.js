a92c827817f22e7ed6fb55ca6d971ebcad019bec
Gopi <gopi.govind@olivejar.co.uk>
2024-08-29 11:52:33 +0100

    Fix pipeline 3

--- src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/app.js
+++ src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/app.js
@@ -1,1 +1,7 @@
 // Write your JavaScript code.
+function ShowHideElement (module) {
+    this.module = module
+    this.id = this.module.id
+    this.bodyExpandedClass = 'app-filter-layout--show'
+    this.sectionExpandedClass = 'app-show-hide__section--show'
+}
