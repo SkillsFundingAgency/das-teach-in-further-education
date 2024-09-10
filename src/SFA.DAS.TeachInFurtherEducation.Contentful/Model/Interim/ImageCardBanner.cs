0f569e283e56483c3a24a8a8e6ce2b0849a2fb02
nileshgnagepatil <nileshgnagepatil@outlook.com>
2024-09-10 10:49:13 +0100

    Image Card banner

--- src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/ImageCardBanner.cs
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/ImageCardBanner.cs
@@ -9,6 +9,11 @@ namespace SFA.DAS.TeachInFurtherEducation.Contentful.Model.Interim
     {
         public Document? BannerContents { get; set; }
 
+        public string? BannerText { get; set; }
+
+        public string? BannerLinkText { get; set; }
+
+        public string? BannerLinkSource { get; set; }
         public Asset? BannerImage { get; set; }
     }
 }
