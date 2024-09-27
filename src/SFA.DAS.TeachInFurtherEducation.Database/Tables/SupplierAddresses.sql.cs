4de134a3cf6ca92045b44efd4c35bdd3825afe63
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-09-27 09:15:18 +0100

    database project

---
+++ src/SFA.DAS.TeachInFurtherEducation.Database/Tables/SupplierAddresses.sql.cs
@@ -0,0 +1,22 @@
+﻿CREATE TABLE SupplierAddresses (
+    Id NVARCHAR(450) NOT NULL PRIMARY KEY,
+    OrganisationName NVARCHAR(100) NOT NULL,
+    ParentOrganisation NVARCHAR(100),
+    Type NVARCHAR(100) NOT NULL,
+    Area NVARCHAR(100) NOT NULL,
+    City NVARCHAR(100) NOT NULL,
+    AddressLine1 NVARCHAR(100),
+    AddressLine2 NVARCHAR(100),
+    AddressLine3 NVARCHAR(100),
+    County NVARCHAR(50),
+    Postcode NVARCHAR(10) NOT NULL,
+    Telephone NVARCHAR(100),
+    Website NVARCHAR(100) NOT NULL,
+    Location GEOGRAPHY,
+    LastUpdated DATETIME2 NOT NULL,
+    IsActive BIT NOT NULL
+);
+
+-- Create a non-clustered index on Postcode
+CREATE NONCLUSTERED INDEX IX_SupplierAddresses_Postcode
+ON SupplierAddresses (Postcode);
