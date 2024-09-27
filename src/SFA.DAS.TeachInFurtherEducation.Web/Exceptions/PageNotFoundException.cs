a2f46bc5c5ee6104fcc87d6c82cc733c24db0fcb
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-09-27 18:08:35 +0100

    security fixes and error page fixes

---
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Exceptions/PageNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SFA.DAS.TeachInFurtherEducation.Web.Exceptions
+{
+    public class PageNotFoundException : Exception
+    {
+        public PageNotFoundException(string message) : base(message) { }
+    }
+}
