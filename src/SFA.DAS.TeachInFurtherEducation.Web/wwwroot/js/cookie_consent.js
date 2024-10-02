cfa642c4ea44d726060a9cdb7e2b10922ad16744
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-10-02 17:20:35 +0100

    reduce size of cookie js

--- src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/cookie_consent.js
+++ src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/cookie_consent.js
@@ -1,50 +1,31 @@
-﻿/* istanbul ignore file */
+﻿// cookies
 function saveCookieSettings() {
-    let consentAnalyticsCookieRadioValue = document.querySelector(
-        "input[name=ConsentAnalyticsCookie]:checked"
-    ).value;
-    let consentFunctionalCookieRadioValue = document.querySelector(
-        "input[name=ConsentFunctionalCookie]:checked"
-    ).value;
-
-    createCookie("AnalyticsConsent", consentAnalyticsCookieRadioValue);
-    createCookie("FunctionalConsent", consentFunctionalCookieRadioValue);
-
-    document.getElementById("confirmation-banner").removeAttribute("hidden");
-    window.scrollTo({ top: 0, behavior: "instant" });
+    ["Analytics", "Functional"].forEach(type => {
+        let value = document.querySelector(`input[name=Consent${type}Cookie]:checked`).value
+        createCookie(`${type}Consent`, value)
+    })
+    showBanner("confirmation-banner")
 }
-function acceptCookies(args) {
-    createCookie("DASSeenCookieMessage", true);
-    document.getElementById("cookieConsent").style.display = "none";
-    if (args === true) {
-        createCookie("AnalyticsConsent", true);
-        createCookie("FunctionalConsent", true);
-        document.getElementById("cookieAccept").removeAttribute("hidden");
-    } else {
-        createCookie("AnalyticsConsent", false);
-        createCookie("FunctionalConsent", false);
-        document.getElementById("cookieReject").removeAttribute("hidden");
-    }
+function acceptCookies(acceptAll) {
+    createCookie("DASSeenCookieMessage", true)
+    ["Analytics", "Functional"].forEach(type => createCookie(`${type}Consent`, acceptAll))
+    showBanner(acceptAll ? "cookieAccept" : "cookieReject")
+    document.getElementById("cookieConsent").style.display = "none"
 }
-function createCookie(cookiname, cookivalue) {
-    let date = new Date();
-    date.setFullYear(date.getFullYear() + 1);
-    let expires = "expires=" + date.toGMTString();
-    document.cookie =
-        cookiname + "=" + cookivalue + ";" + expires + ";path=/;Secure";
+function createCookie(name, value) {
+    let expires = new Date(Date.now() + 31536000000).toGMTString() // 1 year
+    document.cookie = `${name}=${value};expires=${expires};path=/;Secure`
 }
-function hideAcceptBanner() {
-    document.getElementById("cookieAccept").setAttribute("hidden", "");
+function showBanner(id) {
+    document.getElementById(id).removeAttribute("hidden")
+    window.scrollTo({ top: 0, behavior: "instant" })
 }
-function hideRejectBanner() {
-    document.getElementById("cookieReject").setAttribute("hidden", "");
+function hideBanner(id) {
+    document.getElementById(id).setAttribute("hidden", "")
 }
-function deleteCookie(cookiname) {
-    document.cookie =
-        cookiname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; Secure"
+function deleteCookie(name) {
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;Secure`
 }
 function deleteAllCookies() {
-    deleteCookie("DASSeenCookieMessage")
-    deleteCookie("AnalyticsConsent")
-    deleteCookie("FunctionalConsent")
+    ["DASSeenCookieMessage", "AnalyticsConsent", "FunctionalConsent"].forEach(deleteCookie)
 }
