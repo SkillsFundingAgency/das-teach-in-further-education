834a9b920fb25cd07231b9ffdcb9fe175867cc97
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-10-11 13:08:56 +0100

    address failing unit test

---
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Data/Models/SqlDbContextConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.TeachInFurtherEducation.Web.Data.Models
+{
+    public class SqlDbContextConfiguration
+    {
+        public string DatabaseConnectionString { get; set; } = "";
+    }
+}
