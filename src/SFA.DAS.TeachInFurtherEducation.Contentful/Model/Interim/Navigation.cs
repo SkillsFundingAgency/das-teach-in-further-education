4f93b6e0064b24103ac15139047a3d565aa90d8d
Gopi <gopi.govind@olivejar.co.uk>
2024-10-22 10:55:24 +0100

    Navigation creation

---
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/Navigation.cs
@@ -0,0 +1,17 @@
+﻿using Contentful.Core.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.TeachInFurtherEducation.Contentful.Model.Interim
+{
+
+    [ExcludeFromCodeCoverage]
+    public class Navigation : BaseModel, IContent
+    {
+        public required string PreviousText { get; set; }
+
+        public required string PreviousTextSource { get; set; }
+
+        public required string? NextText { get; set; }
+        public string? NextTextSource { get; set; }
+    }
+}
