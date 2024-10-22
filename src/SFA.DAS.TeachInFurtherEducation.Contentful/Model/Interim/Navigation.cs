f5a5afc1892a24cd6d5867e76b59993001cbd5ac
Gopi <gopi.govind@olivejar.co.uk>
2024-10-22 11:30:57 +0100

    Navigation creation

--- src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/Navigation.cs
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/Navigation.cs
@@ -3,15 +3,12 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SFA.DAS.TeachInFurtherEducation.Contentful.Model.Interim
 {
-
     [ExcludeFromCodeCoverage]
     public class Navigation : BaseModel, IContent
     {
-        public required string PreviousText { get; set; }
-
-        public required string PreviousTextSource { get; set; }
-
-        public required string? NextText { get; set; }
-        public string? NextTextSource { get; set; }
+        public string? PreviousText { get; init; }
+        public string? PreviousTextSource { get; init; }
+        public string? NextText { get; init; }
+        public string? NextTextSource { get; init; }
     }
 }
