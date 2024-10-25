27cf44d087a9d63cf24a29b95c39aa97e87c3dbf
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-10-25 11:51:28 +0100

    remove test certs

--- genCert.ps1
+++
@@ -1,3 +0,0 @@
-$thumbprint = "CEAC2449D10427A4AC7A6434CB5C6BB9C4DAF6FD"
-$cert = Get-ChildItem -Path cert:\LocalMachine\My\$thumbprint
-Export-Certificate -Cert $cert -FilePath C:\temp\MyCertificate.cer
