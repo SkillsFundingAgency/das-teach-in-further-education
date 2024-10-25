a50f2894b69b06e51323d0b70d61cd42cbbd8a76
spar-tecknuovo <simon.parsons@tecknuovo.com>
2024-10-25 11:14:24 +0100

    add 301 middleware and mapping configuration

---
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Models/301RedirectionModels.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SFA.DAS.TeachInFurtherEducation.Web.Models
+{
+    public class RedirectConfig
+    {
+        public bool AppendReferrerOnQueryString { get; set; } = false;
+        public List<Trigger> Triggers { get; set; } = new List<Trigger>();
+    }
+
+    public class Trigger
+    {
+        public required int Seq { get; set; }
+        public required string Exp { get; set; }
+        public Regex? CompiledExp { get; set; }
+        public List<Rule> Rules { get; set; } = new List<Rule>();
+    }
+
+    public class Rule
+    {
+        public required int Seq { get; set; }
+        public required string Exp { get; set; }
+        public Regex? CompiledExp { get; set; }
+
+        public required string SendTo { get; set; }
+    }
+}
