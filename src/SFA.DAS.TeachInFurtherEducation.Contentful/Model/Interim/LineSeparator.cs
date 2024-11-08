93b305dfd963483ed13797af65b50dda32dc46e7
Gopi <gopi.govind@olivejar.co.uk>
2024-11-08 16:14:09 +0000

    Line seperator

--- src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/LineSeparator.cs
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Model/Interim/LineSeparator.cs
@@ -3,10 +3,11 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SFA.DAS.TeachInFurtherEducation.Contentful.Model.Interim
 {
-
     [ExcludeFromCodeCoverage]
     public class LineSeparator : BaseModel, IContent
     {
         public required string Title { get; set; }
+        public string? TopMargin { get; set; }
+        public string? BottomMargin { get; set; }
     }
 }
