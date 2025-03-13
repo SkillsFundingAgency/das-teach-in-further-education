1f6f26ec882b72250b8445bbb32d20504d1229e8
Ali Reid <ali.reid@live.co.uk>
2025-03-13 15:10:36 +0000

    External link

--- src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/show_hide.js
+++ src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/show_hide.js
@@ -97,3 +97,15 @@ $(document).ready(function () {
         });
     });
 });
+
+window.addEventListener("DOMContentLoaded", e =>
+{
+    document.querySelectorAll('a[href]').forEach(a =>
+    {
+        if (location.hostname == new URL(a.href).hostname)
+            return;
+
+        a.target = "_blank";
+        a.rel = "noreferrer nofollow noopener";
+    });
+});
