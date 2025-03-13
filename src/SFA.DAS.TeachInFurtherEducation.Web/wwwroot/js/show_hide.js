1ee772fec48b4f13887e434270fd70de89bb8645
Ali Reid <ali.reid@live.co.uk>
2025-03-13 15:23:53 +0000

    Update

--- src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/show_hide.js
+++ src/SFA.DAS.TeachInFurtherEducation.Web/wwwroot/js/show_hide.js
@@ -97,15 +97,3 @@ $(document).ready(function () {
         });
     });
 });
-
-window.addEventListener("DOMContentLoaded", e =>
-{
-    document.querySelectorAll('a[href]').forEach(a =>
-    {
-        if (location.hostname == new URL(a.href).hostname)
-            return;
-
-        a.target = "_blank";
-        a.rel = "noreferrer nofollow noopener";
-    });
-});
