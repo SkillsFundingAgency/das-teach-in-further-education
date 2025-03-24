5c8a114acaf2b87f72e14e170e8a3921ae56e116
Ali Reid <ali.reid@live.co.uk>
2025-03-24 09:03:28 +0000

    Move CSP

--- src/SFA.DAS.TeachInFurtherEducation.Web/Startup.cs
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Startup.cs
@@ -257,17 +257,6 @@ namespace SFA.DAS.TeachInFurtherEducation.Web
 
                 endpoints.MapControllers();
             });
-            
-            app.Use(async (context, next) =>
-            {
-                context.Response.Headers!.Append("X-Frame-Options", "SAMEORIGIN");
-                context.Response.Headers!.Append("X-Content-Type-Options", "nosniff");
-                context.Response.Headers!.Append("X-Xss-Protection", "1");
-                context.Response.Headers!.Append("X-Permitted-Cross-Domain-Policies", "none");
-                context.Response.Headers!.Append("Referrer-Policy", "strict-origin-when-cross-origin");      
-                await next.Invoke();
-            });
-            
         }
 
         /// <remarks>
