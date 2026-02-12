24c314f0897a57627b251dd2abf226f27c72ac6d
jphcdcgi <james.pickering@cgi.com>
2026-02-12 17:19:25 +0000

    WIP - disable until todos are explained

--- src/SFA.DAS.TeachInFurtherEducation.Web/Services/ContentfulNavigationService.cs
+++ src/SFA.DAS.TeachInFurtherEducation.Web/Services/ContentfulNavigationService.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 
 // namespace SFA.DAS.TeachInFurtherEducation.Web.Services
 // {
-    // public class ContentfulNavigationService : IContentfulNavigationService 
-    // {
-         // TODOS: 
-        // Fetch menu structure from Contentful
-        // Build hierarchical tree from flat entries
-        // Cache results?
-    // }
+    // TODOS: 
+    // Fetch menu structure from Contentful
+    // Build hierarchical tree from flat entries
+    // Cache results?
+    // public class ContentfulNavigationService : IContentfulNavigationService
+    // {}
 
-    // TODO: Tree Building Logic:
-    // private List<NavigationMenuItem> BuildMenuTree(List<NavigationMenuItem> allItems, string parentId = null)
-    // {
-    //     return allItems
-    //         .Where(item => item.ParentItem?.Sys?.Id == parentId)
-    //         .OrderBy(item => item.SortOrder)
-    //         .Select(item => new NavigationMenuItem
-    //         {
-    //             ...item, Children = BuildMenuTree(allItems, item.Id)
-    //         })
-    //         .ToList();
-    // }
+// TODO: Tree Building Logic:
+//     private List<NavigationMenuItem> BuildMenuTree(List<NavigationMenuItem> allItems, string parentId = null)
+//     {
+//         return allItems
+//             .Where(item => item.ParentItem?.Sys?.Id == parentId)
+//             .OrderBy(item => item.SortOrder)
+//             .Select(item => new NavigationMenuItem
+//             {
+//                 ...item, Children = BuildMenuTree(allItems, item.Id)
+//             })
+//             .ToList();
+//     }
 // }
