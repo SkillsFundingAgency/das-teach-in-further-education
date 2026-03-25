ebc7b5d9e1de9e80cbe8d0c81a263cfe9efa56b2
BAGHA <Amrit.BAGHA@EDUCATION.GOV.UK>
2026-03-25 13:34:38 +0000

    FEAT: Add Contentful dynamic navigation changes.

---
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Services/Interfaces/IContentfulNavigationService.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.TeachInFurtherEducation.Contentful.Model.Content.Navigation;
+using SFA.DAS.TeachInFurtherEducation.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.TeachInFurtherEducation.Contentful.Services.Interfaces
+{
+    public interface IContentfulNavigationService
+    {
+        Task<NavigationMenuTree?> GetMenuTreeByTitleAsync(string navigationMenuTitle, CancellationToken ct =  default);
+        Task<NavigationMenuTree?> GetPreviewMenuTreeByTitleAsync(string navigationMenuTitle, CancellationToken ct = default);
+    }
+}
