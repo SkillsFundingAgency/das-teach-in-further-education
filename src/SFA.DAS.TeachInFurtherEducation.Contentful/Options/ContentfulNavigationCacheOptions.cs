31573d4ce7ff0b6b1cdc10610fcb21723afcc959
BAGHA <Amrit.BAGHA@EDUCATION.GOV.UK>
2026-03-25 13:34:38 +0000

    FEAT: Add Contentful dynamic navigation changes.

---
+++ src/SFA.DAS.TeachInFurtherEducation.Contentful/Options/ContentfulNavigationCacheOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.TeachInFurtherEducation.Contentful.Options
+{
+    [ExcludeFromCodeCoverage]
+    public sealed class ContentfulNavigationCacheOptions
+    {
+        public required int HeaderMenuCacheTTLMinutes { get; set; }
+    }
+}
